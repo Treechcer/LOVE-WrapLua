faf948cd68c424e589cd537be7f4bf1f3257b197
treecher <treechcer@gmail.com>
2026-01-14 21:40:46 +0100

    added compatibility for non PSP consoles

--- LOVE-WrapLua/OneLua/timer.lua
+++ LOVE-WrapLua/OneLua/timer.lua
@@ -13,7 +13,16 @@ function love.timer.getDelta()
     return dt
 end
 
+local os = love.system.getOS()
 
-function love.timer.getFPS()
-    return screen.fps()
+if os == "PSP" then
+    function love.timer.getFPS()
+        return screen.fps()
+    end
+elseif os == "PS3" or os == "Vita" then
+    function love.timer.getFPS()
+        --haven't gotten really to PS3 or Vita, so it returns this random number
+        --next console I'll port is PS3 btw
+        return 123456
+    end
 end
